b8466c1fbba031828bf46f51f7dbd451a6f42f77
410902054 <71751283+410902054@users.noreply.github.com>
2020-09-30 15:26:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let degrees = 0
 input.onGesture(Gesture.Shake, function () {
     degrees = randint(80, 100)
+    basic.showNumber(degrees)
     if (degrees < 84) {
         basic.showString("A-")
     } else if (degrees >= 85) {
